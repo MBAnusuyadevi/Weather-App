4e66447395012904476aa1baa4dd45574325ad9b
MBAnusuyadevi <anusuyadevi.mb2000@gmail.com>
2023-10-19 09:52:35 +0530

    commit

---
+++ WeatherAPI/purgecss.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    content: ['src\app\Component\weather-search\weathersearch.component.html', 'src\app\Component\weather-search\weathersearch.component.ts'],
+    css: ['src\app\Component\weather-search\weathersearch.component.css']
+  };
